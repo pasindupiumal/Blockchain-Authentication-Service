add777cb196f1a1aa617ccec44b0dca3cfb772ed
pasindu <pasindu0996@gmail.com>
2020-11-05 14:36:00 +0530

    Optimizations

--- contracts/BlockAuthVerify.sol
+++ contracts/BlockAuthVerify.sol
@@ -1,23 +1,27 @@
 pragma solidity ^0.5.16;
 
+//Implementation of the smart contract for maintaining relevant and required data of users.
 contract BlockAuthVerify {
 
+    //Variables and maps declarations
     address payable owner;
     mapping ( address => string ) usernames;
     mapping ( string => address ) reverse_usernames;
     mapping ( string => string ) block_auth_urls;
     mapping ( string => string ) public_keys;
-    mapping ( string => string ) data;
 
+    //Declaration of events. Used to identify the success of specific operations
     event NewUserAdded(address from, string username, bool success);
     event NewVerification(address from, string username, string verificationType, bool success);
     event UserDataUpdated(address from, string username, bool success);
 
+    //Default constructor. Obtains the Ethereum address of the account invoking the smart contract and assigning to owner.
     constructor() public {
 
         owner = msg.sender;
     }
 
+    //Destructor of the smart contract.
     function killBlockAuth() public {
 
         if(msg.sender == owner){
@@ -25,6 +29,11 @@ contract BlockAuthVerify {
         }
     }
 
+    /*
+        Method for inserting a new user to the chain. Obtains relevant information as parameters.
+        Registers the new user with the platform if not already registered.
+        Emits the NewUserAdded event to signal the status of the addNewUser operation.
+    */
     function addNewUser(string memory username, string memory block_auth_url, string memory public_key) public returns (bool) {
 
         if (reverse_usernames[username] == 0x0000000000000000000000000000000000000000){
@@ -42,6 +51,10 @@ contract BlockAuthVerify {
         return false;
     }
 
+    /*
+        Method for determining whether a specific username is already registered in the blockchain.this
+        Takes the username in question as a parameter and returns boolean.
+    */
     function usernameAvailable(string memory username) public view returns (bool) {
 
         if (reverse_usernames[username] == 0x0000000000000000000000000000000000000000) {
@@ -53,36 +66,37 @@ contract BlockAuthVerify {
         }
     }
 
+    /*
+        Method to obtain the username associated with a specific blockchain address.
+        Takes the blockchain address as an input parameter and returns the username if located.
+    */
     function getUsername(address user_address) public view returns (string memory) {
         return usernames[user_address];
     }
 
+    /*
+        Method to obtain the blockchain address associated with a specific username.
+        Takes the username as an input parameter and returns the ethereum address associated.
+    */
     function getAddress(string memory username) public view returns (address) {
         return reverse_usernames[username];
     }
 
+    /*
+        Method to obtain the unique url generated for each user.
+        Takes the username as an input parameter and returns the url associated with that username.
+    */
     function getBlockAuthUrl(string memory username) public view returns (string memory) {
         return block_auth_urls[username];
     }
 
+    /*
+        Method to obtain the public key associated with a specific username.
+        This public key refers to the public key of the RSA key pair generated for each new user.
+        Takes the username as an input parameter and returns the public key assocaited.
+    */
     function getBlockAuthPublicKey(string memory username) public view returns (string memory) {
         return public_keys[username];
     }
-
-    function setEncryptionData(string memory encryptedData) public returns (bool) {
-        if(keccak256(abi.encodePacked(msg.sender)) != keccak256(abi.encodePacked(""))){
-            data[getUsername(msg.sender)] = encryptedData;
-            emit UserDataUpdated(msg.sender, getUsername(msg.sender), true);
-            return true;
-        }
-
-        emit UserDataUpdated(msg.sender, getUsername(msg.sender), false);
-        return false;
-    }
-
-    function getEncryptedData(string memory username) public view returns (string memory) {
-        return data[username];
-    }
-
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   networks: {
-    
+
     development: {
 
       host: "127.0.0.1",
